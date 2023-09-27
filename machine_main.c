626e18955daf9e669146639e2a5b747d994072d1
BrinkAD1 <144182054+BrinkAD1@users.noreply.github.com>
2023-09-26 21:47:40 -0400

    styled trace output more correctly. added DIV function and put fp, sp, and gp registers to correct starting values

--- machine_main.c
+++ machine_main.c
@@ -17,29 +17,15 @@ static union mem_u {
      bin_instr_t instrs[MEMORY_SIZE_IN_WORDS];
 } memory;
 
-<<<<<<< HEAD
 void InstrPrint(bin_instr_t bi, unsigned int i)
 {
     printf("   %d %s", i, instruction_assembly_form(bi));
-=======
-void disasmTextSection(BOFFILE bf, BOFHeader bh)
-{
-    printf(".text %u\n", bh.text_start_address);
-    printf(".text %u\n", bh.text_length); 
-}
-
-
-void InstrPrint(bin_instr_t bi, unsigned int i)
-{
-    printf("%d:\t%s", i, instruction_assembly_form(bi));
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
     printf("\n");
 }
 
 
 int main(int argc, char *argv[]) 
 {
-<<<<<<< HEAD
     word_type register_array[NUM_REGISTERS];
    
     // [0] = PC, [1] = HI, [2] = LO
@@ -47,21 +33,10 @@ int main(int argc, char *argv[])
 
     //initialize register array to 0
     for(int i =0; i < NUM_REGISTERS; i ++ )
-=======
-    word_type register_array[32];
-
-    //initialize register array to 0
-    for(int i =0; i < 32; i ++ )
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
     {
         register_array[i] = 0;
     }
 
-<<<<<<< HEAD
-=======
-    //initialize PC to 0
-    int PC = 0;
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
     //default tracing is off
     int trace = 1;                
 
@@ -72,11 +47,7 @@ int main(int argc, char *argv[])
 
         BOFHeader boffile_header = bof_read_header(boffile);
 
-<<<<<<< HEAD
         int length = boffile_header.text_length / BYTES_PER_WORD; 
-=======
-        int length = boffile_header.text_length / BYTES_PER_WORD;
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
 
         printf("Addr Instruction\n");
         for(int i = 0; i < length; i++)
@@ -84,11 +55,7 @@ int main(int argc, char *argv[])
             InstrPrint( instruction_read(boffile), i*BYTES_PER_WORD);
         }
 
-<<<<<<< HEAD
         printf("    %u:\n", boffile_header.data_start_address);
-=======
-        printf("\t%u\n", boffile_header.data_start_address);
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
     }
 
     if(argc == 2)
@@ -97,7 +64,6 @@ int main(int argc, char *argv[])
 
         BOFHeader boffile_header = bof_read_header(boffile);
 
-<<<<<<< HEAD
         //set the $gp rgister to the start address of data section
         register_array[28] = boffile_header.data_start_address;
 
@@ -119,21 +85,12 @@ int main(int argc, char *argv[])
             if(trace)
             {
                 printf("PC: %d", special_register_array[0]);
-=======
-        int length = boffile_header.text_length / BYTES_PER_WORD;
-        for(int i =0; i < length ; i++)
-        {
-            if(trace)
-            {
-                printf("PC: %d", PC);
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
                 for(int i =0; i < 32; i ++ )
                 {
                     
                     if(i % 6 ==0)
                         printf("\n");
                         
-<<<<<<< HEAD
                     printf("GPR[%s]: %d   ", regname_get(i), register_array[i]);
                 }
                 printf("\n");
@@ -148,24 +105,6 @@ int main(int argc, char *argv[])
                 case(reg_instr_type):
                     //printf("reg_instr_type\n");
                     //printf("reg.func: %u reg.rd: %u reg.rs %u reg.rt: %u reg.shift: %u\n",bi.reg.func, bi.reg.rd, bi.reg.rs, bi.reg.rt, bi.reg.shift);
-=======
-                    printf("%s: %d\t", regname_get(i), register_array[i]);
-                }
-            }
-
-            bin_instr_t bi = instruction_read(boffile);
-
-            instr_type instr = instruction_type(bi);
-
-            switch(instr)
-            {
-                //increment PC on each call
-                
-
-                case(reg_instr_type):
-                    printf("reg_instr_type\n");
-                    printf("reg.func: %u reg.rd: %u reg.rs %u reg.rt: %u reg.shift: %u\n",bi.reg.func, bi.reg.rd, bi.reg.rs, bi.reg.rt, bi.reg.shift);
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
                     switch(bi.reg.func)
                     {
                         case(ADD_F):
@@ -175,7 +114,6 @@ int main(int argc, char *argv[])
                             register_array[bi.reg.rd] = register_array[bi.reg.rs] - register_array[bi.reg.rt];
                             break;
                         case(MUL_F):
-<<<<<<< HEAD
                             unsigned int result = register_array[bi.reg.rs] * register_array[bi.reg.rt];
                             // Put LSB into LO
                             unsigned int lsb_mask = (1u << 16) - 1; // Create a mask with 16 LSBs
@@ -198,14 +136,6 @@ int main(int argc, char *argv[])
                             break;
                         case(MFLO_F):
                             register_array[bi.reg.rd] = special_register_array[2];
-=======
-                            break;
-                        case(DIV_F):
-                            break;
-                        case(MFHI_F):
-                            break;
-                        case(MFLO_F):
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
                             break;
                         case(AND_F):
                             register_array[bi.reg.rd] = register_array[bi.reg.rs] & register_array[bi.reg.rt];
@@ -232,13 +162,8 @@ int main(int argc, char *argv[])
                     }
                     break;
                 case(syscall_instr_type):
-<<<<<<< HEAD
                     //printf("syscall_instr_type\n");
                     //printf("code: %u func: %u op: %u\n", bi.syscall.code, bi.syscall.func, bi.syscall.op);
-=======
-                    printf("syscall_instr_type\n");
-                    printf("code: %u func: %u op: %u\n", bi.syscall.code, bi.syscall.func, bi.syscall.op);
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
                     switch(bi.syscall.code)
                     {
                         case(exit_sc):
@@ -254,25 +179,16 @@ int main(int argc, char *argv[])
                             printf("read char sc\n");
                             break;
                         case(start_tracing_sc):
-<<<<<<< HEAD
                             //printf("start tracing sc\n");
                             trace = 1;
                             break;
                         case(stop_tracing_sc):
                             //printf("stop char sc\n");
-=======
-                            printf("start tracing sc\n");
-                            trace = 1;
-                            break;
-                        case(stop_tracing_sc):
-                            printf("stop char sc\n");
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
                             trace = 0;
                             break;
                     }
                     break;
                 case(immed_instr_type):
-<<<<<<< HEAD
                     //printf("immed_inst_type\n");
                     //printf("op: %u rs: %u rt: %u\n",bi.immed.op, bi.immed.rs, bi.immed.rt);
                     switch(bi.immed.op)
@@ -288,22 +204,6 @@ int main(int argc, char *argv[])
                             break;
                         case(XORI_O):
                             register_array[bi.immed.rt] = register_array[bi.immed.rs] ^ machine_types_zeroExt(bi.immed.immed);
-=======
-                    printf("immed_inst_type\n");
-                    printf("op: %u rs: %u rt: %u\n",bi.immed.op, bi.immed.rs, bi.immed.rt);
-                    switch(bi.immed.op)
-                    {
-                        case(REG_O):
-                            break;
-                        case(ADDI_O):
-                            register_array[bi.immed.rt] = bi.immed.immed + register_array[bi.immed.rs];
-                            break;
-                        case(ANDI_O):
-                            break;
-                        case(BORI_O):
-                            break;
-                        case(XORI_O):
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
                             break;
                         case(BEQ_O):
                             break;
@@ -318,10 +218,7 @@ int main(int argc, char *argv[])
                         case(BNE_O):
                             break;
                         case(LBU_O):
-<<<<<<< HEAD
                             
-=======
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
                             break;
                         case(LW_O):
                             break;
@@ -329,44 +226,25 @@ int main(int argc, char *argv[])
                             break;
                         case(SW_O):
                             break;
-<<<<<<< HEAD
                     }
                     break;
                 case(jump_instr_type):
 
                     switch(bi.jump.op)
                     {
-=======
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
                         case(JMP_O):
                             break;
                         case(JAL_O):
                             break;
                     }
                     break;
-<<<<<<< HEAD
-=======
-                case(jump_instr_type):
-                    printf("jump_inst_type\n");
-                    break;
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
                 case(error_instr_type):
                     return 1;
                     break;
             }
-<<<<<<< HEAD
 
             //increment PC for next instruction
             special_register_array[0] += 4;     
         }
     }  
 }
-=======
-            
-            
-            //increment PC for next instruction
-            PC = PC + 4;     
-        }
-    }  
-}
->>>>>>> 23d0c8d9ae6ea5fe37facffad6145337eed6cb0d
